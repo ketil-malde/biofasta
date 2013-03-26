db025719a5ed2f3eaedfc574a968a71e4c65f5d8
Ketil Malde <ketil@malde.org>
2013-03-26 10:12:56 +0100

    Add support for Qual data (from old biolib)
    Ignore-this: cbaf9bdafc389eec43e87b78d6aa284b
    
    darcs-hash:403323763da4051419ed6cde4bd3085806ffc29d

--- src/Bio/Sequence/Fasta.hs
+++ src/Bio/Sequence/Fasta.hs
@@ -11,6 +11,10 @@ module Bio.Sequence.Fasta
     ( Sequence(..)
     -- * Reading and writing plain FASTA files
     , readFasta, writeFasta, hReadFasta, hWriteFasta
+    -- * Reading and writing quality files
+    , readQual, writeQual, hWriteQual
+    -- * Combining FASTA and quality files
+    , readFastaQual, hWriteFastaQual, writeFastaQual
     -- * Counting sequences in a FASTA file
     , countSeqs
     -- * Helper function for reading your own sequences
@@ -20,7 +24,7 @@ module Bio.Sequence.Fasta
 ) where
 
 
-import Data.Char (chr, isSpace)
+import Data.Char (isSpace)
 import Data.List (groupBy, intersperse)
 import System.IO
 
@@ -38,10 +42,10 @@ data Sequence = Seq SeqLabel SeqData (Maybe QualData)
                 deriving (Show, Eq)
 
 instance BioSeq Sequence where
-  seqid     (Seq lab seq mqual) = SeqLabel {unSL = B.takeWhile (/= ' ') $ unSL lab}
-  seqheader (Seq lab seq mqual) = lab
-  seqdata   (Seq lab seq mqual) = seq
-  seqlength (Seq lab seq mqual) = Offset {unOff = B.length $ unSD seq}
+  seqid     (Seq lab _sq _mqual) = SeqLabel {unSL = B.takeWhile (/= ' ') $ unSL lab}
+  seqheader (Seq lab _sq _mqual) = lab
+  seqdata   (Seq _lab sq _mqual) = sq
+  seqlength (Seq _lab sq _mqual) = Offset {unOff = B.length $ unSD sq}
 
 toStr :: SeqData -> String
 toStr  = B.unpack . unSD
@@ -49,6 +53,50 @@ toStr  = B.unpack . unSD
 -- | Lazily read sequences from a FASTA-formatted file
 readFasta :: FilePath -> IO [Sequence]
 readFasta f = (mkSeqs . B.lines) `fmap` B.readFile f
+
+-- | Read quality data for sequences to a file.
+readQual :: FilePath -> IO [Sequence]
+readQual f = (mkQual . B.lines) `fmap` B.readFile f
+
+-- | Write quality data for sequences to a file.
+writeQual :: FilePath -> [Sequence] -> IO ()
+writeQual f ss = do
+  h <- openFile f WriteMode
+  hWriteQual h ss
+  hClose h
+
+-- | Read sequence and associated quality.  Will error if
+--   the sequences and qualites do not match one-to-one in sequence.
+readFastaQual :: FilePath -> FilePath -> IO [Sequence]
+readFastaQual  s q = do
+  ss <- readFasta s
+  qs <- readQual q
+  -- warning: assumes correct qual file!
+  let mkseq s1@(Seq x y _) s2@(Seq _ _ (Just z))
+          | seqid s1 /= seqid s2 = error ("mismatching sequence and quality: "
+                                                ++B.unpack (unSL $ seqid s1)++","++B.unpack (unSL $ seqid s2))
+          | B.length (unSD y) /= B.length (unQD z)   = error ("mismatching sequence and quality lengths:"
+                                                ++ B.unpack (unSL $ seqid s1)++","++show (B.length (unSD y),B.length (unQD z)))
+          | otherwise   = Seq x y (Just z)
+      mkseq _ _ = error "readFastaQual: could not combine Fasta and Qual information"
+      zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+      zipWith' _ [] [] = []
+      zipWith' _ _ _ = error "readFastaQual: Unbalanced sequence and quality"
+  return $ zipWith' mkseq ss qs
+
+-- | Write sequence and quality data simulatnously
+--   This may be more laziness-friendly.
+writeFastaQual :: FilePath -> FilePath -> [Sequence] -> IO ()
+writeFastaQual f q ss = do
+  hf <- openFile f WriteMode
+  hq <- openFile q WriteMode
+  hWriteFastaQual hf hq ss
+  hClose hq
+  hClose hf
+
+hWriteFastaQual :: Handle -> Handle -> [Sequence] -> IO ()
+hWriteFastaQual hf hq = mapM_ wFQ
+    where wFQ s = wFasta hf s >> wQual hq s
 
 -- | Write sequences to a FASTA-formatted file.
 --   Line length is 60.
@@ -78,6 +126,29 @@ wFasta h (Seq l d _) = do
   let ls = splitsAt 60 (unSD d)
   mapM_ (B.hPut h) $ intersperse (B.pack "\n") ls
   B.hPut h $ B.pack "\n"
+
+mkQual :: [ByteString] -> [Sequence]
+mkQual = map f . blocks
+    where f [] = error "mkQual on empty input - this is impossible"
+          f (l:ls) = Seq (SeqLabel (B.drop 1 l)) (SeqData B.empty)
+                     (Just $ QualData $ BB.pack $ go 0 ls)
+          isDigit x = x <= 58 && x >= 48 
+          go i (s:ss) = case BB.uncons s of Just (c,rs) -> if isDigit c then go (c - 48 + 10*i) (rs:ss)
+                                                           else let rs' = BB.dropWhile (not . isDigit) rs
+                                                                in if BB.null rs' then i : go 0 ss else i : go 0 (rs':ss)
+                                            Nothing -> i : go 0 ss
+          go _ [] = []
+
+hWriteQual :: Handle -> [Sequence] -> IO ()
+hWriteQual h = mapM_ (wQual h)
+
+wQual :: Handle -> Sequence -> IO ()
+wQual h (Seq l _ (Just (QualData q))) = do
+  wHead h l
+  let qls = splitsAt 20 q
+      qs = B.pack . unwords . map show . BB.unpack
+  mapM_ ((\l' -> B.hPut h l' >> B.hPut h (B.pack "\n")) . qs) qls
+wQual _ (Seq _ _ Nothing) = return ()
 
 -- | Convert a list of FASTA-formatted lines into a list of sequences.
 --   Blank lines are ignored.
